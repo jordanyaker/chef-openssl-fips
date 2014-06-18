60eeb97d869ce6b6dafbad8e72d708255db5d557
Jordan Yaker <jordan.e.yaker@gmail.com>
2014-06-17 22:26:50 -0400

    Added FIPS prefix.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@
 fips_version = '2.0.5'
 default['openssl_fips']['fips']['version']  = fips_version
 default['openssl_fips']['fips']['url']      = "https://www.openssl.org/source/openssl-fips-#{fips_version}.tar.gz"
+default['openssl_fips']['fips']['prefix']  = "/opt/openssl-fips-#{fips_version}"
 default['openssl_fips']['fips']['checksum'] = '54ad8102f73488e9f34d3143b55866a147582810'
 
 openssl_version = '1.0.1g'
